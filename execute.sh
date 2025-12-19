e98512f61483f676b206239fc16ba6c278e99dca
Manex Aranzadi <manexae@protonmail.com>
2025-12-19 10:35:21 +0100

    en teoria deberia de matarlo, pero no lo hace

--- execute.sh
+++ execute.sh
@@ -7,10 +7,28 @@ pip install -r requirements.txt
 python3 export.py
 python3 main.py
 
+
+PORT=8000
+fuser -k $PORT/tcp 2>/dev/null
+
+
 cd webPage/src
-python3 -m http.server 8000 &
+python3 -m http.server $PORT &
 
 SERVER_PID=$!
+
+
+
+
+cleanup() {
+    echo "Deteniendo servidor..."
+    kill $SERVER_PID
+    exit 0
+}
+
+trap cleanup INT
+
+
 sleep 1
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
@@ -21,3 +39,6 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 else
     echo "Sistema operativo no soportado para abrir automáticamente el navegador."
 fi
+
+
+wait $SERVER_PID
