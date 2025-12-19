a437363000f1f860838d3ae0e6461eecf948f2a4
Manex Aranzadi <manexae@protonmail.com>
2025-12-19 10:35:11 +0100

    abre el server en linux i mac

--- execute.sh
+++ execute.sh
@@ -6,3 +6,18 @@ pip install -r requirements.txt
 
 python3 export.py
 python3 main.py
+
+cd webPage/src
+python3 -m http.server 8000 &
+
+SERVER_PID=$!
+sleep 1
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    open http://localhost:8000
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    xdg-open http://localhost:8000
+else
+    echo "Sistema operativo no soportado para abrir automáticamente el navegador."
+fi
