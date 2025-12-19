8765950f7f6bac155f149673fc92bcefb2fb9d77
Manex Aranzadi <manexae@protonmail.com>
2025-12-19 10:03:21 +0100

    añade ahora las demas cartas por si acaso

--- webPage/src/main.js
+++ webPage/src/main.js
@@ -16,21 +16,14 @@ fetch("./cards.json")
 function renderDeck(deckData) {
   const commanderNode = document.getElementById('commander');
   const conjuntoNode = document.getElementById('conjunto');
+  const otrosNode = document.getElementById('noIncluidas');
+
 
   // Renderizamos el comandante
   const commander = deckData.commander;
   const commanderCard = cardsDB[commander.id] || {};
   
-  commanderNode.innerHTML = `
-    <div class='bg-gray-600 p-5 rounded-md'>
-      <img src='${commanderCard.image_url || ""}' 
-           class='h-90 w-auto rounded-lg hover:scale-110 transition delay-150 duration-300 ease-in-out'>
-      <h1 class='mt-1.5 text-center text-amber-50'>
-        ${commander.name} - Score: ${commander.score}
-      </h1>
-      
-    </div>
-  `;
+  commanderNode.innerHTML = imagen(commanderCard, commander) ;
 
   // Renderizamos el resto del deck
     deckData.deck
@@ -38,17 +31,34 @@ function renderDeck(deckData) {
     .sort((a, b) => b.score - a.score)
     .forEach(c => {
     const carta = cardsDB[c.id] || {};
-    conjuntoNode.innerHTML += `
-      <div class='bg-gray-600 p-5 rounded-md'>
+    conjuntoNode.innerHTML += imagen(carta, c) ;
+  });
+
+
+  deckData.other_cards
+    .filter(c => c.score !== 0)
+    .sort((a, b) => b.score - a.score)
+    .forEach(c => {
+    const carta = cardsDB[c.id] || {};
+    otrosNode.innerHTML += imagen(carta, c) ;
+  });
+}
+
+
+function imagen(carta, c){
+    valor= `
+      <div class='bg-gray-600 p-5 rounded-md flex flex-col items-center justify-center'>
         <img src='${carta.image_url || ""}' 
-             class='h-90 w-auto rounded-lg hover:scale-110 transition delay-150 duration-300 ease-in-out'>
+             class='h-90 rounded-lg hover:scale-110 transition delay-150 duration-300 ease-in-out'>
         <h1 class='mt-1.5 text-center text-amber-50'>
           ${c.name} - Score: ${c.score} ${c.included ? "(Incluida)" : "(No incluida)"}
         </h1>
       </div>
     `;
-  });
+
+    return valor
 }
+
 
 //          PARA VER LAS ESTADISTICAS INTERNAS
 //          <pre class='text-white'>${JSON.stringify(c.score_breakdown, null, 2)}</pre>
