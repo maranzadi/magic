f36b5edf8fe8162367f209fb116ef98adee019f4
Manex Aranzadi <manexae@protonmail.com>
2025-12-22 22:52:36 +0100

    generado la funcion pa conseguir al commandante pues

--- webPage/src/main.js
+++ webPage/src/main.js
@@ -64,6 +64,20 @@ function imagen(c){
     return valor
 }
 
+function commandante(c){
+    valor= `
+      <div class='bg-gray-600 p-5 rounded-md flex flex-col items-center justify-center'>
+        <img src='${c.image_url || ""}' 
+             class='h-90 rounded-lg hover:scale-110 transition delay-150 duration-300 ease-in-out'>
+        <h1 class='mt-1.5 text-center text-amber-50'>
+          ${c.name} - Score: ${c.score} ${c.included ? "(Incluida)" : "(No incluida)"}
+        </h1>
+      </div>
+    `;
+
+    return valor
+}
+
 
 //          PARA VER LAS ESTADISTICAS INTERNAS
 //          <pre class='text-white'>${JSON.stringify(c.score_breakdown, null, 2)}</pre>
